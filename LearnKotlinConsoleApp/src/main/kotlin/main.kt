54b1661c6ccbde8670ee9fa366d09b07f2f06229
LastLight <lastlightstudios17@gmail.com>
2020-11-25 22:47:40 -0700

    working function
    TODO: read in int only

--- LearnKotlinConsoleApp/src/main/kotlin/main.kt
+++ LearnKotlinConsoleApp/src/main/kotlin/main.kt
@@ -1,9 +1,13 @@
 package com.mypackage
 
+fun timeInMinutesForResin(resin: Int): Int {
+    return resin * 8;
+}
 
 fun main(args: Array<String>) {
     println("Welcome")
-    println("Please state your name")
-    val name= readLine()
-    println("Hello $name!")
+    println("How much Resin do you need")
+    val inResin = 50
+    var timeForResin = timeInMinutesForResin(inResin).toString()
+    println("It will take $timeForResin minutes")
 }
