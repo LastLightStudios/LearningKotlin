f7dd92dc36b81305d50b63b007bc3e40aed53298
LastLight <lastlightstudios17@gmail.com>
2020-11-25 23:28:50 -0700

    It works as long as you put in a number

--- LearnKotlinConsoleApp/src/main/kotlin/main.kt
+++ LearnKotlinConsoleApp/src/main/kotlin/main.kt
@@ -1,4 +1,5 @@
 package com.mypackage
+import java.util.Scanner
 
 fun timeInMinutesForResin(resin: Int): Int {
     return resin * 8;
@@ -6,8 +7,15 @@ fun timeInMinutesForResin(resin: Int): Int {
 
 fun main(args: Array<String>) {
     println("Welcome")
-    println("How much Resin do you need")
-    val inResin = 50
+    println("How much Resin do you need?")
+    val scanner = Scanner(System.`in`)
+    var inResin = scanner.nextInt()
+    while (inResin < 1){
+        println("Enter a positive number")
+        println("How much Resin do you need?")
+        inResin = scanner.nextInt()
+    }
+
     var timeForResin = timeInMinutesForResin(inResin).toString()
     println("It will take $timeForResin minutes")
 }
